41c6af3b33404d53388d5000c02429d144308d1a
BillYoungRC <130173562+BillYoungRC@users.noreply.github.com>
2026-05-29 18:51:37 -0400

    Create deploy.ps1

---
+++ deploy.ps1
@@ -0,0 +1,15 @@
+$Source = $env:GITHUB_WORKSPACE
+$destination = "C:\Python\pipedrive-end-of-month-dave\"
+$sleepTimer = 1
+
+if (Test-Path $destination) {
+    Write-Host "Removing from $destination"
+    Remove-Item -Path $destination\* -Recurse -Force
+}
+
+New-Item -ItemType Directory -Path "$destination\logs" -Force
+
+Write-Host "Deploying from $Source to $destination"
+Copy-Item -Path $Source\* -Destination $destination -Recurse -Force
+
+Write-Host "Deploy complete."
